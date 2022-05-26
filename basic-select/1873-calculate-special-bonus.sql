635c44358868f7cda4319c9f5152302bd26c4d61
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-05-25 19:10:19 -0600

    Changing condition to =1 it is faster
    Runtime: 548 ms, faster than 71.15% of MySQL online submissions for Calculate Special Bonus.
    Memory Usage: 0B, less than 100.00% of MySQL online submissions for Calculate Special Bonus.

--- basic-select/1873-calculate-special-bonus.sql
+++ basic-select/1873-calculate-special-bonus.sql
@@ -1,4 +1,4 @@
 SELECT employee_id, (CASE
-            WHEN employee_id%2 != 0 And name NOT LIKE 'M%' THEN salary
+            WHEN employee_id%2 = 1 And name NOT LIKE 'M%' THEN salary
             ELSE 0 END) AS bonus
 FROM employees;
