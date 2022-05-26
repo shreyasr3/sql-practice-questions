de042cacc3dc51546712f586f5fcd6d26335c013
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-05-26 13:41:09 -0600

    1484 Group Sold Products By the Date
    Runtime: 369 ms, faster than 90.61% of MySQL online submissions for Group Sold Products By The Date.
    Memory Usage: 0B, less than 100.00% of MySQL online submissions for Group Sold Products By The Date.

---
+++ basic-select/1484-group-sold-products-by-the-date.sql
@@ -0,0 +1,12 @@
+/* My SQL */
+
+SELECT sell_date, count(DISTINCT product) AS num_sold, 
+    GROUP_CONCAT(DISTINCT product ORDER BY product) AS products
+FROM activities
+GROUP BY sell_date 
+ORDER BY sell_date;
+
+
+
+
+/*In MSSQL there is no ORDER BY clause in GROUP_CONCAT*/
