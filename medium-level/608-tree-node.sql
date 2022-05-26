08ceea1cab27fe6abcfca31a1ed9e61c1fe1b64a
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-05-26 15:18:59 -0600

    608 Tree Node LeetCode Medium
    Runtime: 470 ms, faster than 44.10% of MySQL online submissions for Tree Node.
    Memory Usage: 0B, less than 100.00% of MySQL online submissions for Tree Node.

---
+++ medium-level/608-tree-node.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT t1.id, (CASE
+            WHEN t1.p_id IS NULL THEN 'Root'
+            WHEN t2.id IS NULL THEN 'Leaf'
+            ELSE 'Inner' END) AS type    
+FROM tree t1
+LEFT JOIN tree t2
+ON t1.id = t2.p_id
+ORDER BY t1.id
+;
+
