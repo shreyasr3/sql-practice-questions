a99b2539e412d8061cd09b158202401c3aeeb302
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-05-25 18:50:00 -0600

    adding Union in the query

--- basic-select/weather-observation-station-5.sql
+++ basic-select/weather-observation-station-5.sql
@@ -1,7 +1,7 @@
 /* MS SQL */
 
 SELECT TOP 1 city, LEN(city) FROM station 
-ORDER BY len(city) ASC, city ASC; 
-
+ORDER BY len(city) ASC, city ASC
+UNION
 SELECT TOP 1 city, LEN(city) FROM station
 ORDER BY len(city) DESC, city ASC; 
