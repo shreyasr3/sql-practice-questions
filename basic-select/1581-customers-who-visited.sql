fd8a758d4469f99db72e5efbc4c976130d57eb96
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-05-27 15:20:33 -0600

    With Join
    Runtime: 1247 ms, faster than 52.19% of MySQL online submissions for Customer Who Visited but Did Not Make Any Transactions.
    Memory Usage: 0B, less than 100.00% of MySQL online submissions for Customer Who Visited but Did Not Make Any Transactions.

--- basic-select/1581-customers-who-visited.sql
+++ basic-select/1581-customers-who-visited.sql
@@ -3,3 +3,14 @@ FROM visits
 WHERE visit_id NOT IN (SELECT DISTINCT visit_id FROM transactions)
 GROUP BY customer_id
 ;
+
+
+
+/*With JOin query is faster than sub-query*/
+
+SELECT customer_id, COUNT(v.visit_id) AS count_no_trans 
+FROM visits AS v
+NATURAL LEFT JOIN transactions AS t
+WHERE t.visit_id IS NULL
+GROUP BY customer_id
+;
