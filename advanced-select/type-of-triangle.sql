cf7bb3b52dc7fd1f605c9e634beac0a2f4a50211
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-06-04 12:02:20 -0600

    Correcting Syntax error

--- advanced-select/type-of-triangle.sql
+++ advanced-select/type-of-triangle.sql
@@ -1,4 +1,4 @@
-SELECT CASE (
+SELECT (CASE
     WHEN (A+B <= C OR B+C <= A OR A+C <= B) THEN 'Not A Triangle'
     WHEN (A = B AND B = C) THEN 'Equilateral'
     WHEN (A = B AND B != C) OR (B = C AND C != A) OR (A = C AND A != B) 
