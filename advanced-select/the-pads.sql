ee23dbf3316ec824b1ad5ea9f164a24e0b16f566
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-06-05 13:01:42 -0600

    The PADS

---
+++ advanced-select/the-pads.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT(name,'(',SUBSTRING(occupation,1,1),')')
+FROM occupations
+ORDER BY name
+;
+SELECT CONCAT('There are a total of ',COUNT(name),' ',lower(occupation),'s.')
+FROM occupations
+GROUP BY occupation
+ORDER BY COUNT(name), occupation
+;
