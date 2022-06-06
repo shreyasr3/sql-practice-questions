36d449ec751d16c07356f8d2d1c5230a6d43086f
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-06-05 19:09:13 -0600

    Update 1084-sales-analysis-III.sql

--- basic-select/1084-sales-analysis-III.sql
+++ basic-select/1084-sales-analysis-III.sql
@@ -8,3 +8,17 @@ AND P.product_id NOT IN (
                     WHERE sale_date NOT BETWEEN  '2019-01-01' AND '2019-03-31'
                         )
 ;
+
+
+
+/* SMART SOLUTION */
+
+SELECT s.product_id, product_name
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id
+GROUP BY s.product_id
+HAVING MIN(sale_date) >= CAST('2019-01-01' AS DATE) AND
+       MAX(sale_date) <= CAST('2019-03-31' AS DATE)
+       ;
+       
