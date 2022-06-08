37ccac59c808f6370cba84fd1ca0991be5ffb707
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-06-08 12:48:07 -0600

    Update 178-rank-scores.sql

--- advanced-select/178-rank-scores.sql
+++ advanced-select/178-rank-scores.sql
@@ -2,3 +2,13 @@ SELECT score,
     DENSE_RANK() OVER(ORDER BY score DESC) AS rank
 FROM Scores
 ;
+
+
+/* MySQL Syntax */
+
+SELECT score,
+    DENSE_RANK() OVER W AS 'rank'
+FROM Scores
+WINDOW W AS (ORDER BY score DESC)
+;
+
