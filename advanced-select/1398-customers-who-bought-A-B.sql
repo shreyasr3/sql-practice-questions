c09622bd9d3b2479b3fb715ccb61e61e4696cc6e
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-06-24 13:35:49 -0600

    1398. Customers Who Bought A and B but Not C
    Runtime: 2358 ms, faster than 5.03% of MySQL online submissions for Customers Who Bought Products A and B but Not C.
    Memory Usage: 0B, less than 100.00% of MySQL online submissions for Customers Who Bought Products A and B but Not C.

---
+++ advanced-select/1398-customers-who-bought-A-B.sql
@@ -0,0 +1,9 @@
+SELECT C.customer_id, C.customer_name
+FROM Customers AS C
+    JOIN Orders AS O
+    ON C.customer_id = O.customer_id
+GROUP BY C.customer_id
+HAVING SUM(O.product_name = 'A') > 0 AND 
+        SUM(O.product_name = 'B') > 0 AND
+        SUM(O.product_name = 'C') = 0
+;
