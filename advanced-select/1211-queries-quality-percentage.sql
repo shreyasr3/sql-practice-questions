54aaba3891a8a7fafea5efa22d4122531ba80b08
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-06-24 12:07:10 -0600

    1211. Queries Quality and Percentage
    Runtime: 526 ms, faster than 98.50% of MySQL online submissions for Queries Quality and Percentage.
    Memory Usage: 0B, less than 100.00% of MySQL online submissions for Queries Quality and Percentage.

---
+++ advanced-select/1211-queries-quality-percentage.sql
@@ -0,0 +1,21 @@
+SELECT 
+    query_name,
+    ROUND(SUM(rating / position) / COUNT(*) ,2) AS quality,
+    ROUND(100 * SUM(rating < 3) / COUNT(*) , 2) AS poor_query_percentage
+FROM 
+  Queries
+GROUP BY 
+  query_name
+;
+
+
+
+SELECT 
+	query_name,
+	ROUND(AVG(rating / position), 2) AS quality,
+	ROUND(AVG(rating < 3) * 100, 2) AS poor_query_percentage 
+FROM 
+	Queries
+GROUP BY 
+	query_name
+;
