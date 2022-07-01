7d73c0ebfbebcf5cb7bd4113a2534469020b500b
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-07-01 16:45:06 -0600

    Update 1747-leetflex-banned-accounts.sql

--- advanced-select/1747-leetflex-banned-accounts.sql
+++ advanced-select/1747-leetflex-banned-accounts.sql
@@ -4,3 +4,15 @@ JOIN LogInfo AS B
 ON A.account_id=B.account_id AND A.ip_address != B.ip_address
 AND (B.login BETWEEN A.login AND A.logout)
 ;
+
+
+
+
+## WithoutJoin
+
+select account_id
+from loginfo a
+where exists (select 1 from loginfo 
+              where a.account_id = account_id 
+              and a.login between login and logout and a.ip_address != ip_address) 
+group by account_id
